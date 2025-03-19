bb2328b1d066f6984936be8947716205bc91c047
yappiverse <setiawannaufal0@gmail.com>
2025-03-19 19:44:17 +0700

    add init for route init

---
+++ backend/routes/__init__.py
@@ -0,0 +1,6 @@
+from .classify import router as classify_router
+from .labels import router as labels_router
+from .logs import router as logs_router
+from .uploads import router as upload_router
+from .health import router as health_router
+from .files import router as files_router
